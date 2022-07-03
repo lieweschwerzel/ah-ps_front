23675a688ec65862594d4c7375b2f8b02a18faa9
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2022-07-03 21:25:53 +0200

    searchbar and itemListView

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import SearchBar from "./Components/SearchBar";
+import BookData from "./Data.json";
+import ItemView from './Components/ItemListView';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [ItemList, setItemList] = useState([{}])
+  const [mail, setMail] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [msg, setMsg] = useState(false)
+
+    // Read all items by mail axios.get(`https://price-tracker-back.herokuapp.com/api/item/${mail}`)
+    useEffect(() => {
+      document.title = "AH Price Tracker"
+      if (mail !== '') {
+        axios.get(`http://localhost:8000/subs/${mail}`)
+          .then(res => {
+            setItemList(res.data)
+            console.log(res.data)
+          })
+      }
+    }, [mail, msg]);
+
+
+  const changeMessage = () => { msg ? setMsg(false) : setMsg(true) }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar placeholder="Enter a Book Name..." data={BookData} />
+      <span className="card-text">
+            <input className="mb-3 form-control desIn" onChange={event => setMail(event.target.value)} placeholder='Email' required />
+          </span>
+      <div className="item-list-view">
+          <ItemView test={changeMessage} ItemList={ItemList} />
+        </div>
     </div>
   );
 }
---
+++ src/Components/Item.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function TrackedItem(props) {
+    const deleteitemHandler = (email, product_name) => {
+        axios.delete(`http://localhost:8000/subs/${email}/${product_name}`)
+            .then(res => {
+                console.log("Clicked from Grandchild");
+                props.test();
+            })
+    }
+
+    if (!props.item.email ) {
+        return (
+            <div>
+                <p>
+                    <span>
+                    </span>                            
+                </p>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div>
+                <p> 
+                   <button onClick={() => deleteitemHandler(props.item.email, props.item.product_name)} className="btn btn-outline-danger my-2 mx-2" style={{ borderRadius: '15px'}}>X</button>
+                    <span style={{ fontWeight: 'bold, underline' , flex: 1, flexDirection: 'row'}}>
+                        <img src={props.item.img_url} alt="ahimage" style={{width: '100px', height: '50%'}}/>
+                       
+                    {props.item.product_name}: €</span>{props.item.email}
+                    
+                </p>
+            </div>
+        )
+    }
+
+
+}
+
+export default TrackedItem;
