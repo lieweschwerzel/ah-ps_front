adbf8b231a95e867ae07484de5cd855e0a62a79f
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2022-08-11 12:57:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [mail, setMail] = useState('')
   const [loading, setLoading] = useState(false)
   const [msg, setMsg] = useState(false)
-  const url = `https://ah-ps-spring-boot.herokuapp.com}`; //`http://localhost:8080`
+  const url = `https://ah-ps-spring-boot.herokuapp.com`; //`http://localhost:8080`
 
   // Read all items by mail axios.get(`https://price-tracker-back.herokuapp.com/api/item/${mail}`)
   useEffect(() => {
