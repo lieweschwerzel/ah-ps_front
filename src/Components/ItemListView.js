c38a1fdeac4657f9c10638c626fb2ffe6fbbd4cc
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2022-08-15 17:51:13 +0200

    css

--- src/Components/ItemListView.js
+++ src/Components/ItemListView.js
@@ -3,12 +3,16 @@ import TrackedItem from './Item'
 
 
 export default function ItemView(props) {
+    console.log("pop"+Array.isArray(props.ItemList) && props.ItemList.lenght)
     return (
-        
+
         <div  >
-            <ul>
+            {(Array.isArray(props.ItemList)) && (
+                <ul>
                 {props.ItemList.map((item, key) => <TrackedItem item={item} mail={props.mail} refresh={props.refresh} />)}
             </ul>
+            )}
+            
         </div>
     )
 }
